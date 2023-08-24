74e44e18e92a3e9f9ae03bc52c9815e77f8d8ef0
Dhruv4171 <parwanidhruv0990@gmail.com>
2023-08-24 18:58:40 +0530

    Commit for seprate student role components

--- src/components/studentrole/index.jsx
+++ src/components/studentrole/index.jsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import { services } from '../../services'
 
-const StudentRole = ({section,setSection}) => {
-  const [sections,setSections] = useState('')
+const StudentRole = ({ section, setSection }) => {
+  const [sections, setSections] = useState([])
   useEffect(() => {
     services.getSections()
-        .then(response => {
-    
-            setSections(response.data);
-        })
-        .catch(error => {
-            console.error('Error fetching sections:', error);
-        });
-}, []);
+      .then(response => {
+        setSections(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching sections:', error);
+      });
+  }, []);
   return (
     <div>
-  <select required value={section} onChange={(e)=>setSection(e.target.value)}>
-      <option value='' selected disabled>--SelectYear--</option>
-      {sections.map(section => <option value={section.id}>{section.name}</option>)}
-  </select>
+      <select required value={section} onChange={(e) => setSection(e.target.value)}>
+        <option value='' selected disabled>--SelectYear--</option>
+        {
+          sections.map(sec => {
+            return (<option value={sec.id}>
+              {sec.name}
+            </option>)
+          })
+        }
+      </select>
     </div>
   )
 }
