cf74eb8e355cc8daf4dfbf1ef29111cce5e5dee3
Anish-uii <anishchoudhary9285@gmail.com>
2023-08-25 14:28:31 +0530

    Added Student Panel

--- src/components/studpanel/index.jsx
+++ src/components/studpanel/index.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Wrapper } from './style'
 
 const StudPanel = () => {
+  const user = JSON.parse(window.localStorage.getItem("userinfo")).user
+  console.log(user)
   return (
-    <div>
-      <h1>Student Panel</h1>
-    </div>
+    <Wrapper>
+
+            <div className="inner">
+                <div key={user.id} className='user_container'>
+                    <div className='personal_container'>
+                        <div className='img_container'>
+                            <h1>Welcome</h1>
+                            <img src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='user_img' />
+                        </div>
+                        <div className='details container'>
+                            <p className='user_info'>{user.name}</p>
+                            <p className='user_info'>{user.email}</p>
+                            <p className='user_info'>{user.sectionId}</p>
+                        </div>
+                        <div className='panel_btn'>
+                            <form>
+                              <input type="button" value="View Attendance" />
+                              <input type="button" value="" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Wrapper>
   )
 }
 
---
+++ src/components/studpanel/style.js
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+
+export const Wrapper = styled.section`
+
+img{
+    widht:150px;
+    border-radius:50%;
+    height:150px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+`
