aaa1bfcc04ca94b1deb616e47ba56225c165a227
Dhruv4171 <parwanidhruv0990@gmail.com>
2023-08-26 16:07:34 +0530

    Commit for student panel img

--- src/components/studpanel/index.jsx
+++ src/components/studpanel/index.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React ,{ useState , useEffect } from 'react'
 import { Wrapper } from './style'
 import { useNavigate } from 'react-router'
+import axios from 'axios'
 
 const StudPanel = () => {
-  const user = JSON.parse(window.localStorage.getItem("userinfo")).user
+  const [users, setUsers] = useState([]);
+  const [curruser, setCurruser] = useState({});
+  const userContact = JSON.parse(window.localStorage.getItem('userinfo')).user.contact;
   const navigate = useNavigate()
+    var foundUser = null;
+
+    useEffect(() => {
+        axios.get('https://quizattendace.onrender.com/api/user/read')
+            .then((res) => {
+                setUsers(res.data);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    }, []);
+
+    useEffect(() => {
+        foundUser = users.find(user => user.contact === userContact);
+        if (foundUser) {
+            setCurruser(foundUser);
+        }
+    }, [users, userContact]);
 
   const displayAttd = () => {
     navigate('/attdlist')
@@ -14,19 +35,21 @@ const StudPanel = () => {
     navigate('/updateprofile')
   }
 
+  const imgSrc =curruser.img ? curruser.img : 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60';
+
   return (
     <Wrapper>
 
             <div className="inner">
-                <div key={user.id} className='user_container'>
+                <div key={curruser.id} className='user_container'>
                     <div className='personal_container'>
                         <div className='img_container'>
                             <h1>Welcome</h1>
-                            <img src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='user_img' />
+                            <img src={imgSrc} alt='user_img' />
                         </div>
                         <div className='details container'>
-                            <h2 className='user_info'>{user.name}</h2>
-                            <p className='user_info'>{user.sectionId}</p>
+                            <h2 className='user_info'>{curruser.name}</h2>
+                            <p className='user_info'>{curruser.sectionId}</p>
                         </div>
                         <div className='panel_btn'>
                             <form>
