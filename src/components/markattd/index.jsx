a1e0eb6b7d4cd011d8c12d253dff376b4858edaa
Anish-uii <anishchoudhary9285@gmail.com>
2023-08-29 13:29:11 +0530

    Updates in MarkAttendance

--- src/components/markattd/index.jsx
+++ src/components/markattd/index.jsx
@@ -41,13 +41,30 @@ const MarkAttd = () => {
             });
     };
 
+    const [studentsPresentCount, setStudentsPresentCount] = useState(0);
+    const [studentsAbsentCount, setStudentsAbsentCount] = useState(0);
+
+    useEffect(() => {
+        const presentCount = attendance.filter(status => status).length;
+        const absentCount = attendance.length - presentCount;
+        setStudentsPresentCount(presentCount);
+        setStudentsAbsentCount(absentCount);
+    }, [attendance]);
+
     const toggleAttendance = (studentIndex, status) => {
         const updatedAttendance = [...attendance];
         updatedAttendance[studentIndex] = status;
         setAttendance(updatedAttendance);
         setCurrentStudentIndex(prevIndex => prevIndex + 1);
     };
+
     const currentStudent = students[currentStudentIndex];
+
+    const retakeAttendance = () => {
+        setAttendance(new Array(students.length).fill(false));
+        setCurrentStudentIndex(0);
+    };
+
     return (
         <Wrapper>
             <div className="inner">
@@ -56,17 +73,17 @@ const MarkAttd = () => {
                         <div className="student-row" key={currentStudent.id}>
                             <div className="student-info">
                                 <img src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8VXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' alt={currentStudent.name} />
-                                <p>{currentStudent.name}</p>
+                                <h2>{currentStudent.name}</h2>
                             </div>
                             <div className="attendance-buttons">
                                 <button
-                                    className={attendance[currentStudentIndex] === true ? "present" : ""}
+                                    className={attendance[currentStudentIndex] ? "present" : ""}
                                     onClick={() => toggleAttendance(currentStudentIndex, true)}
                                 >
                                     Present
                                 </button>
                                 <button
-                                    className={attendance[currentStudentIndex] === false ? "absent" : ""}
+                                    className={!attendance[currentStudentIndex] ? "absent" : ""}
                                     onClick={() => toggleAttendance(currentStudentIndex, false)}
                                 >
                                     Absent
@@ -75,7 +92,27 @@ const MarkAttd = () => {
                         </div>
                     )}
                     {currentStudentIndex === students.length && (
-                        <button className="mark-button" onClick={markAttendance}>Submit Attendance</button>
+                        <div className='final-attendance'>
+                            <div className='attendance-summary'>
+                                <h3>Attendance Summary</h3>
+                                <p>Students Present: {studentsPresentCount}</p>
+                                <p>Students Absent: {studentsAbsentCount}</p>
+                                <p>Attendance List: </p>
+                                <ul>
+                                    {students.map((student, index) => (
+                                        <li key={student.id}>
+                                            {student.name}: {attendance[index] ? "Present" : "Absent"}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className='submitattd-buttons'>
+                            <input type="button" value="Retake Attendance" className='retake-attendance' onClick={retakeAttendance}  />
+                            <input type="button" value="Submit Attendance" className="mark-input" onClick={markAttendance} />
+                            </div>
+                        </div>
+
+
                     )}
                 </div>
             </div>
--- src/components/markattd/style.js
+++ src/components/markattd/style.js
@@ -1,6 +1,75 @@
 import { styled } from "styled-components";
 
 const Wrapper = styled.div`
+height: 100vh;
+padding: 30px 10px;
 
+.student-info{
+    padding: 5px 10px;
+}
+        .student-info img{
+            display: block;
+            margin: 40px auto;
+            width: 50%;
+            border-radius: 50%;  
+        }
+        h2 {
+            padding: 5px 10px;  
+            text-align: center;
+            font-size: 1.4rem;   
+        }
+        .attendance-buttons{
+            padding: 10px 30px;
+            display: flex;
+            justify-content: space-evenly;
+        }
+            .attendance-buttons>button{
+                width: 40%;
+                border-radius: 5px;
+                padding: 5px 5px;
+                // width : 200px;
+                margin : 30px auto;
+                display : block;
+                border : none;
+                background : #292;
+                color : #fff;
+                font-weight : bold;
+                border-bottom : solid 3px #070;
+                
+            }
+            .attendance-buttons>button:hover{
+                cursor: pointer;
+            }
+            .final-attendance{
+                padding: 10px 30px;
+            }
+            .final-attendance h3{
+                text-align: center;
+            }
+            .attendance-summary p, ul{
+                margin-left: 20px;
+            }
+            .attendance-summary ul{
+                list-style: none;
+                padding: 5px;
+            }
+            .final-attendance input[type=button]{
+                paddin: 2px 3px;
+                width : 40%;
+                margin : 30px auto;
+                border : none;
+                background : #292;
+                color : #fff;
+                font-weight : bold;
+                border-bottom : solid 3px #070;
+                border-radius : 3px;
+            }
+            .submitattd-buttons{
+                display: flex;
+                justify-content: space-evenly;
+            }
+            .final-attendance input[type=button]:hover{
+                cursor:pointer;
+            }
 `
 export default Wrapper
